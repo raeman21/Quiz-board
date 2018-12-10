734046d6066030c5f59d7816b18a8d28e85b7b86
raeman21 <mandelar94@gmail.com>
2018-12-10 08:00:00 +0300

    replace different names in radio input with same name

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var answers = 5
 var rightAnswer = 20
 var wrongAnswer = 0
-var totalPoints = display(rightAnswer/100)
-  var result = multiply(rightAnswer,answer)
+var totalPoints = display(rightAnswer / 100)
+var result = multiply(rightAnswer, answer)
