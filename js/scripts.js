cb9a5d18e60f0695f102bd36837f0129e7dca965
raeman21 <mandelar94@gmail.com>
2018-12-10 16:44:35 +0300

    add total points variable, function of percentage

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,9 @@ var answer= $("input:radio[name=answer1,answer2,answer3,answer4,answer1qn5]:chec
 
     })
   }*/
-var answers=["true/false","Method","Lower Camel case","var","quotes"]
+var answers=["true/false","Method","Lower Camel case","var","quotes"];
+var totalPoints=20;
+
+function percentage(score){
+  return "Your score is"+ parseInt((score/100)+%)
+}
