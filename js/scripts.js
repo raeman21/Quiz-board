2624f8b04b4a75e9f12c6872cf8ef50fc9dfb026
raeman21 <mandelar94@gmail.com>
2018-12-10 16:31:04 +0300

    change values from numbers to appropriate names

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-var answers = 5
-var rightAnswer = 20
-var wrongAnswer = 0
-var totalPoints = display(rightAnswer / 100)
-var result = multiply(rightAnswer, answer)
+/*
+var rightAnswer= document.getElementById('#right').val(20);
+var wrongAnswer= document.getElementById('#wrong').val(0);
+var answer= $("input:radio[name=answer1,answer2,answer3,answer4,answer1qn5]:checked").val(20)
+
+
+  $(document).ready(function){
+    $("button").submit(function(event){
+      event.preventDefault();
+      alert("totalPoints")
+
+    })
+  }*/
