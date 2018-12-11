271b691d575b4a62f4c29269a18b7a0609b6b56c
raeman21 <mandelar94@gmail.com>
2018-12-11 09:19:25 +0300

    update JS to fix syntax errors

--- js/scripts.js
+++ js/scripts.js
@@ -11,31 +11,31 @@ var answer= $("input:radio[name=answer1,answer2,answer3,answer4,answer1qn5]:chec
 
     })
   }*/
-var answers=["true/false","Method","Lower Camel case","var","quotes"];
-var eachPoint=20;
+var answers = ["true/false", "Method", "Lower Camel case", "var", "quotes"];
+var eachPoint = 20;
 
-function percentage(score){
-  return "Your score is"+ parseInt((score/100)+ %;
+function percentage(score) {
+  return "Your score is" + parseInt((score / 100) + '%');
 }
 
 
 
-$('.submit').click(function(event){
+$('#button').click(function(event) {
   event.preventDefault();
 
   $('#totalPoints').text('');
-  var score=0;
-  var answerOne=($("input[type=radio][name=qn1]:checked").val());
-  var answerTwo=($("input[type=radio][name=qn2]:checked").val());
-  var answerThree=($("input[type=radio][name=qn3]:checked").val());
-  var answerFour=($("input[type=radio][name=qn4]:checked").val());
-  var answerFive=($("input[type=radio][name=qn5]:checked").val());
-});
+  var score = 0;
+  var answerOne = ($("input[type=radio][name=qn1]:checked").val());
+  var answerTwo = ($("input[type=radio][name=qn2]:checked").val());
+  var answerThree = ($("input[type=radio][name=qn3]:checked").val());
+  var answerFour = ($("input[type=radio][name=qn4]:checked").val());
+  var answerFive = ($("input[type=radio][name=qn5]:checked").val());
+})
 if (answerOne === undefined || answerTwo === undefined || answerThree === undefined) {
-    $('#triviaIncomplete').text('Kindly Complete Trivia Before Submitting');
-    $('#triviaIncomplete').fadeOut(10000);
-    else if (answerOne === answers[1]) {
-      score += eachPoint;
+  $('#triviaIncomplete').text('Kindly Complete Trivia Before Submitting');
+  $('#triviaIncomplete').fadeOut(10000);
+} else if (answerOne === answers[1]) {
+  score += eachPoint;
 }
 if (answerTwo === answers[2]) {
   score += eachPoint;
@@ -45,8 +45,9 @@ if (answerThree === answer[3]) {
 }
 if (answerFour === answer[4]) {
   score += eachPoint;
+}
 if (answerFive === answer[5]) {
   score += eachPoint;
-}
+
 
 }
