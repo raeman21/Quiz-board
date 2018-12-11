0a852c6f1a1051431b8f19ce1b4a5017e4857f0e
raeman21 <mandelar94@gmail.com>
2018-12-11 11:00:26 +0300

    css slash remove, js update

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,29 @@
+/*
+var rightAnswer= document.getElementById('#right').val(20);
+var wrongAnswer= document.getElementById('#wrong').val(0);
+var answer= $("input:radio[name=answer1,answer2,answer3,answer4,answer1qn5]:checked").val(20)
 
+
+  $(document).ready(function){
+    $("button").submit(function(event){
+      event.preventDefault();
+      alert("totalPoints")
+
+    })
+  }*/
 var answers=["true/false","Method","Lower Camel case","var","quotes"];
 var eachPoint=20;
 
 function percentage(score){
-  return "Your score is"+ parseInt((score/100));
+  return "Your score is"+ parseInt((score/100)+"%");
 }
 
 
- $(document).ready(function() {
-   $('.submit').click(function(event){
-     event.preventDefault();
-});
 
-  $('#totalPoints').text('.');
+$('.submit').click(function(event){
+  event.preventDefault();
+
+  $('#totalPoints').text('');
   var score=0;
   var answerOne=($("input[type=radio][name=qn1]:checked").val());
   var answerTwo=($("input[type=radio][name=qn2]:checked").val());
@@ -23,7 +34,7 @@ function percentage(score){
 if (answerOne === undefined || answerTwo === undefined || answerThree === undefined) {
     $('#triviaIncomplete').text('Kindly Complete Trivia Before Submitting');
     $('#triviaIncomplete').fadeOut(10000);
-   } else if (answerOne === answers[1]) {
+    else if (answerOne === answers[1]) {
       score += eachPoint;
 }
 if (answerTwo === answers[2]) {
@@ -35,7 +46,7 @@ if (answerThree === answer[3]) {
 if (answerFour === answer[4]) {
   score += eachPoint;
 if (answerFive === answer[5]) {
-  score += eachPoint;
+  score += eachPoint
 }
 
 }
