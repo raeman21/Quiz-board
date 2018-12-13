7e20cd0aca8c28f105925b9c0dac369b28d47ca3
raeman21 <mandelar94@gmail.com>
2018-12-13 10:31:02 +0300

    change background color to image, remove most jquery functionality, simplify JS variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,38 @@
-/*
-var rightAnswer= document.getElementById('#right').val(20);
-var wrongAnswer= document.getElementById('#wrong').val(0);
-var answer= $("input:radio[name=answer1,answer2,answer3,answer4,answer1qn5]:checked").val(20)
+var buttonClick = function() {
+  var answers = ["true/false", "Method", "Lower Camel case", "var", "quotes"];
+  var eachPoint = 20;
+  var score = 0;
+  var answerOne = ($("input[type=radio][id=rightAnswer1]:checked").val());
+  var answerTwo = ($("input[type=radio][name=qn2]:checked").val());
+  var answerThree = ($("input[type=radio][name=qn3]:checked").val());
+  var answerFour = ($("input[type=radio][name=qn4]:checked").val());
+  var answerFive = ($("input[type=radio][name=qn5]:checked").val());
 
+  var totalPoints = function percentage(score) {
 
-  $(document).ready(function){
-    $("button").submit(function(event){
-      event.preventDefault();
-      alert("totalPoints")
+    if (answerOne === undefined || answerTwo === undefined || answerThree === undefined)
 
-    })
-  }*/
-var answers=["true/false","Method","Lower Camel case","var","quotes"];
-var eachPoint=20;
+      if (answerOne === answers[1]) {
+        score += eachPoint;
 
-function percentage(score){
-  return "Your score is"+ parseInt((score/100)+"%");
-}
-
-
-
-$('.submit').click(function(event){
-  event.preventDefault();
-
-  $('#totalPoints').text('');
-  var score=0;
-  var answerOne=($("input[type=radio][name=qn1]:checked").val());
-  var answerTwo=($("input[type=radio][name=qn2]:checked").val());
-  var answerThree=($("input[type=radio][name=qn3]:checked").val());
-  var answerFour=($("input[type=radio][name=qn4]:checked").val());
-  var answerFive=($("input[type=radio][name=qn5]:checked").val());
-});
-if (answerOne === undefined || answerTwo === undefined || answerThree === undefined) {
-    $('#triviaIncomplete').text('Kindly Complete Trivia Before Submitting');
-    $('#triviaIncomplete').fadeOut(10000);
-    else if (answerOne === answers[1]) {
+      }
+    if (answerTwo === answers[2]) {
       score += eachPoint;
-}
-if (answerTwo === answers[2]) {
-  score += eachPoint;
-}
-if (answerThree === answer[3]) {
-  score += eachPoint;
-}
-if (answerFour === answer[4]) {
-  score += eachPoint;
-if (answerFive === answer[5]) {
-  score += eachPoint
-}
+    }
+    if (answerThree === answer[3]) {
+      score += eachPoint;
+    }
+    if (answerFour === answer[4]) {
+      score += eachPoint;
+    }
+    if (answerFive === answer[5]) {
+      score += eachPoint
+    }
 
-}
+    $(document).ready.function() {
+      $("#button").click(dunction(
+        alert("buttonClick")
+      ))
+    }
+  };
+};
