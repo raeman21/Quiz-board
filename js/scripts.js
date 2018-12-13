2c2af1ca4b87e1243c260f460879b8368f7442e9
raeman21 <mandelar94@gmail.com>
2018-12-13 14:09:28 +0300

    fixed JS and JQuery functionality, added .hide().show()functions and display panel

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,40 @@
-var buttonClick = function() {
-  var answers = ["true/false", "Method", "Lower Camel case", "var", "quotes"];
-  var eachPoint = 20;
-  var score = 0;
-  var answerOne = ($("input[type=radio][id=rightAnswer1]:checked").val());
-  var answerTwo = ($("input[type=radio][name=qn2]:checked").val());
-  var answerThree = ($("input[type=radio][name=qn3]:checked").val());
-  var answerFour = ($("input[type=radio][name=qn4]:checked").val());
-  var answerFive = ($("input[type=radio][name=qn5]:checked").val());
+$(document).ready(function() {
+  $("#button").click(function() {
 
-  var totalPoints = function percentage(score) {
+    var answer = []
+    answer.push($("input:radio[name=qn1]:checked").val())
+    answer.push($("input:radio[name=qn2]:checked").val())
+    answer.push($("input:radio[name=qn3]:checked").val())
+    answer.push($("input:radio[name=qn4]:checked").val())
+    answer.push($("input:radio[name=qn5]:checked").val())
 
-    if (answerOne === undefined || answerTwo === undefined || answerThree === undefined)
+    var score = 0;
 
-      if (answerOne === answers[1]) {
-        score += eachPoint;
 
-      }
-    if (answerTwo === answers[2]) {
-      score += eachPoint;
-    }
-    if (answerThree === answer[3]) {
-      score += eachPoint;
-    }
-    if (answerFour === answer[4]) {
-      score += eachPoint;
-    }
-    if (answerFive === answer[5]) {
-      score += eachPoint
-    }
+    if (answer[0] === "20") {
+      score += 20;
 
-    $(document).ready.function() {
-      $("#button").click(dunction(
-        alert("buttonClick")
-      ))
     }
-  };
-};
+    if (answer[1] === "20") {
+      score += 20;
+
+    }
+    if (answer[2] === "20") {
+      score += 20;
+
+    }
+    if (answer[3] === "20") {
+      score += 20;
+
+    }
+    if (answer[4] === "20") {
+      score += 20;
+
+    }
+    $(".display").val(score);
+  });
+  $("#button").click(function() {
+    $('.well').hide()
+    $(".totalResult").show()
+  });
+});
